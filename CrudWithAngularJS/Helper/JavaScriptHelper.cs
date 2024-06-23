8298e34e538d6f09aecc0f2c0014565eab8f2a7e
SatishGuptag <141490903+SatishGuptaG@users.noreply.github.com>
2024-06-23 17:20:02 +0530

    AddPaginationToUseAngular-
    AddPaginationToUseAngular-

--- CrudWithAngularJS/Helper/JavaScriptHelper.cs
+++ CrudWithAngularJS/Helper/JavaScriptHelper.cs
@@ -6,6 +6,8 @@ using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
+using CrudWithAngularJS.Models;
 
 namespace CrudWithAngularJS.Helper
 {
@@ -38,6 +40,37 @@ namespace CrudWithAngularJS.Helper
 			string json = JsonConvert.SerializeObject(model, settings);
 
 			return json;
+		}
+		// Method to convert an object model and return an object with keys in camelCase
+		public static JToken ConvertToCamelCaseObject(object model)
+		{
+			// Configure JsonSerializerSettings to use camelCase for property names
+			var settings = new JsonSerializerSettings
+			{
+				ContractResolver = new CamelCasePropertyNamesContractResolver()
+			};
+
+			// Serialize the model object to JSON string with camelCase property names
+			string json = JsonConvert.SerializeObject(model, settings);
+
+			// Deserialize the JSON string back to a JToken
+			JToken camelCaseToken = JsonConvert.DeserializeObject<JToken>(json);
+
+			return camelCaseToken;
+		}
+		public static JsonResult ConvertAndReturnJson(string jsonString)
+		{
+			// Deserialize JSON string to List<Product>
+			var products = JsonConvert.DeserializeObject<List<Product>>(jsonString);
+
+			// Convert list to JSON with camelCase property names
+			var camelCaseJson = ConvertToJson(products);
+
+			return new JsonResult
+			{
+				Data = camelCaseJson,
+				JsonRequestBehavior = JsonRequestBehavior.AllowGet
+			};
 		}
 
 	}
